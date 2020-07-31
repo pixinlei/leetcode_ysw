6aa4755b864a8f6fe24477b25d9989ac33103abe
pixinlei <2954716615@qq.com>
2020-07-31 17:13:46 +0800

    koko banana

---
+++ 875/875.js
@@ -0,0 +1,14 @@
+//尝试一下 暴力 再优化
+/**
+ * @param {number[]} piles
+ * @param {Number} H
+ * @returns {Number}
+ */
+var minEatingSpeed = function(piles, H){
+  let lo = 1,
+      hi = Math.max(...piles)
+      console.log(lo,hi);
+  //  求出最大香蕉数，在某把中
+  return lo
+}
+minEatingSpeed([3,6,7,11],8)
